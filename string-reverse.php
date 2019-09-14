87143a9852da4c878d40bafa5ba3a0bea8e6f224
Vishal <vishal@incaendo.com>
2019-09-14 23:12:33 +0530

    Updated

--- string-reverse.php
+++ string-reverse.php
@@ -1,22 +1,11 @@
 <?php
-
-	echo '<pre>';
-	print_r($_SERVER);
-	echo '</pre>';
-
-
-	$url = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
-	$url .= $_SERVER['SERVER_NAME'];
-
-	echo $url;
-
 	
 
 
-
 	$config['base_url'] = ((isset ( $_SERVER ['HTTPS'] ) && $_SERVER ['HTTPS'] == "on") ? "https" : "http");
+	$config['base_url'] = (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == "ghahealth.herokuapp.com") ? 'https' : $config['base_url'];
 	$config['base_url'] .= "://" . $_SERVER ['HTTP_HOST'];
-	$config['base_url'] .= str_replace ( basename ( $_SERVER ['SCRIPT_NAME'] ), "", $_SERVER ['SCRIPT_NAME'] );
+	$config['base_url'] .= str_replace ( basename ( $_SERVER ['SCRIPT_NAME'] ), "", $_SERVER ['SCRIPT_NAME']);
 
 	echo '<pre>';
 	print_r($config);
